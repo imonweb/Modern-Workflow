9883a8b5228a2b4d6a13ed0833289ea5afd56667
Imon <contact@imonweb.co.uk>
2021-03-10 10:55:55 +0000

    Completed the Header CSS

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,21 @@ import '../styles/styles.css'
 if(module.hot){
   module.hot.accept()
 }
+
+/*  Lesson example code below this line */
+function Person(fullName,favColor) {
+  this.name =  fullName;
+  this.favColor = favColor;
+  this.greet = function(){
+    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favColor + " . ");
+  }
+}
+
+let john = new Person("John Doe","blue");
+john.greet();
+
+let jane = new Person("Jane Doe","green");
+jane.greet();
+
+
+ 
